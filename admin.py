45087cc97c9ed52faf8cee88594200002d450988
Kaspars Sprogis <darklow@gmail.com>
2013-02-12 21:58:53 +0200

    Kitchensink added

--- admin.py
+++ admin.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
-from django.contrib.admin import ModelAdmin
-from django.forms import TextInput, ModelForm, Textarea
-from .models import *
-from suit.widgets import SuitDateWidget
+from django.contrib.admin import ModelAdmin, SimpleListFilter
+from django.contrib.admin.widgets import AdminDateWidget
+from django.forms import TextInput, ModelForm, Textarea, Select
+from .models import Country, Continent, KitchenSink
+from suit.widgets import SuitDateWidget, SuitSplitDateTimeWidget
 from django.utils.translation import ugettext_lazy as _
+
 
 class CountryForm(ModelForm):
     class Meta:
@@ -13,6 +15,19 @@ class CountryForm(ModelForm):
             'code': SmallInput,
             'independence_day': SuitDateWidget,
         }
+
+
+class ContinentAdmin(ModelAdmin):
+    search_fields = ('name',)
+    list_display = ('name_', 'name',)
+    list_editable = ('name',)
+    list_display_links = ('name_',)
+
+    def name_(self, obj):
+        return unicode(obj)
+
+
+admin.site.register(Continent, ContinentAdmin)
 
 
 class CountryAdmin(ModelAdmin):
@@ -25,3 +40,80 @@ class CountryAdmin(ModelAdmin):
 
 
 admin.site.register(Country, CountryAdmin)
+
+
+class CountryFilter(SimpleListFilter):
+    """
+    List filter example that shows only referenced(used) values
+    """
+    title = 'country'
+    parameter_name = 'country'
+
+    def lookups(self, request, model_admin):
+        # You can use also "Country" instead of "model_admin.model"
+        # if this is not direct relation
+        countries = set([c.country for c in model_admin.model.objects.all()])
+        return [(c.id, c.name) for c in countries]
+
+    def queryset(self, request, queryset):
+        if self.value():
+            return queryset.filter(country__id__exact=self.value())
+        else:
+            return queryset
+
+
+class KitchenSinkForm(ModelForm):
+    class Meta:
+        SmallInput = TextInput(attrs={'class': 'input-small'})
+        CompactTextarea = Textarea(attrs={'rows': '2'})
+        widgets = {
+            'multiple2': SmallInput,
+            'date': AdminDateWidget(attrs={'class': 'vDateField input-small'}),
+            'date_widget': SuitDateWidget,
+            'datetime_widget': SuitSplitDateTimeWidget,
+            'textfield': CompactTextarea,
+            'linked_foreign_key': Select(attrs={'class': 'linked-select'}),
+        }
+
+
+class KitchenSinkAdmin(admin.ModelAdmin):
+    raw_id_fields = ()
+    form = KitchenSinkForm
+    # inlines = (FridgeInline, MicrowaveInline)
+    search_fields = ['name']
+    radio_fields = {"horizontal_choices": admin.HORIZONTAL,
+                    'vertical_choices': admin.VERTICAL}
+    list_editable = ('boolean', )
+    list_filter = ('choices', 'date', CountryFilter)
+    readonly_fields = ('readonly_field',)
+    raw_id_fields = ('raw_id_field',)
+    fieldsets = [
+        (None, {'fields': ['name', 'help_text', 'textfield',
+                           ('multiple_in_row', 'multiple2'),
+                           'file', 'readonly_field']}),
+        ('Date and time', {
+            'description': 'Improved date/time widgets (SuitDateWidget, '
+                           'SuitSplitDateTimeWidget) . Uses original JS.',
+            'fields': ['date_widget', 'datetime_widget']}),
+
+        ('Foreign key relations',
+         {'description': 'Original select and linked select feature',
+          'fields': ['country', 'linked_foreign_key', 'raw_id_field']}),
+
+        ('Boolean and choices',
+         {'fields': ['boolean', 'boolean_with_help', 'choices',
+                     'horizontal_choices', 'vertical_choices']}),
+
+        ('Collapsed settings', {
+            'classes': ('collapse',),
+            'fields': ['hidden_checkbox', 'hidden_choice']}),
+        ('And one more collapsable', {
+            'classes': ('collapse',),
+            'fields': ['hidden_charfield', 'hidden_charfield2']}),
+
+    ]
+    list_display = (
+        'name', 'help_text', 'choices', 'horizontal_choices', 'boolean')
+
+
+admin.site.register(KitchenSink, KitchenSinkAdmin)
--- models.py
+++ models.py
@@ -1,9 +1,4 @@
-from datetime import datetime
-from django.contrib.admin.models import LogEntry
-from django.contrib.auth.models import User
-from django.contrib.contenttypes.models import ContentType
-from django.db import models, connections
-from pprint import pprint
+from django.db import models
 
 
 class Continent(models.Model):
@@ -11,6 +6,12 @@ class Continent(models.Model):
 
     def __unicode__(self):
         return self.name
+
+    class Meta:
+        ordering = ['name']
+
+    def save(self, force_insert=False, force_update=False, using=None):
+        return super(Continent, self).save(force_insert, force_update, using)
 
 
 class Country(models.Model):
@@ -29,5 +30,57 @@ class Country(models.Model):
         verbose_name_plural = "Countries"
 
 
-import urllib2
-from django.utils import simplejson
+TYPE_CHOICES = ((1, 'Awesome'), (2, 'Good'), (3, 'Normal'), (4, 'Bad'))
+TYPE_CHOICES2 = ((1, 'Hot'), (2, 'Normal'), (3, 'Cold'))
+TYPE_CHOICES3 = ((1, 'Tall'), (2, 'Normal'), (3, 'Short'))
+
+
+class KitchenSink(models.Model):
+    name = models.CharField(max_length=64)
+    help_text = models.CharField(max_length=64,
+                                 help_text="Enter fully qualified name")
+    multiple_in_row = models.CharField(max_length=64,
+                                       help_text='Help text for multiple')
+    multiple2 = models.CharField(max_length=10, blank=True)
+    textfield = models.TextField(blank=True)
+
+    file = models.FileField(upload_to='.', blank=True)
+    readonly_field = models.CharField(max_length=127, default='Some value here')
+
+    date = models.DateField(blank=True, null=True)
+    date_and_time = models.DateTimeField(blank=True, null=True)
+
+    date_widget = models.DateField(blank=True, null=True)
+    datetime_widget = models.DateTimeField(blank=True, null=True)
+
+    boolean = models.BooleanField(default=True)
+    boolean_with_help = models.BooleanField(
+        help_text="Boolean field with help text")
+
+    horizontal_choices = models.SmallIntegerField(choices=TYPE_CHOICES,
+                                                  default=1,
+                                                  help_text='Horizontal '
+                                                            'choices look '
+                                                            'like this')
+    vertical_choices = models.SmallIntegerField(choices=TYPE_CHOICES2,
+                                                default=2,
+                                                help_text="Some help on "
+                                                          "vertical choices")
+    choices = models.SmallIntegerField(choices=TYPE_CHOICES3,
+                                       default=3,
+                                       help_text="Help text")
+    hidden_checkbox = models.BooleanField()
+    hidden_choice = models.SmallIntegerField(choices=TYPE_CHOICES3,
+                                             default=2, blank=True)
+    hidden_charfield = models.CharField(max_length=64, blank=True)
+    hidden_charfield2 = models.CharField(max_length=64, blank=True)
+
+    country = models.ForeignKey(Country, related_name='foreign_key_country')
+    linked_foreign_key = models.ForeignKey(Country, limit_choices_to={
+        'continent__name': 'Europe'}, related_name='foreign_key_linked')
+    raw_id_field = models.ForeignKey(Country,
+                                     help_text='Regular raw ID field',
+                                     null=True, blank=True)
+
+    def __unicode__(self):
+        return self.name
